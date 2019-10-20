4e18f249a65a2fa0751cb880f9bab17ea69d6afd
coocoeman <54761438+coocoeman@users.noreply.github.com>
2019-10-20 15:05:39 +0800

    移動

---
+++ 2DGame_Run/Assets/EX/Cat.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    #region
+    [Header("角色屬性")]
+    [Tooltip("角色名稱")]public string catName ;
+    [Range(0.1f,5.5f),Tooltip("移動速度")]public float speed = 0.1f ;
+    [Range(1, 5),Tooltip("跳躍次數")]public int jumpNumder = 2;
+    [Range(1f, 3.5f), Tooltip("跳躍高度")]public float jump = 2.5f;
+    [Tooltip("是否離開地面")]public bool jumpBool;
+
+    [Tooltip("角色與攝影機")]public Transform cat ,useCamera;
+
+    
+
+    #endregion
+    private void Start()
+    {
+        //不要問我會怕();
+    }
+    void 不要問我會怕()
+    {
+        Debug.Log(catName);
+        Debug.Log(speed);
+        Debug.Log(jumpNumder);
+        Debug.Log(jump);
+        Debug.Log(jumpBool);
+    }
+
+    private void Update()
+    {
+        catMobile();
+        cameraMobile();
+
+    }
+
+    /// <summary>
+    /// 角色移動
+    /// </summary>
+    void catMobile()
+    {
+        cat.Translate(Vector3.right* Time.deltaTime*speed);
+    }
+
+    /// <summary>
+    /// 攝影機移動
+    /// </summary>
+    void cameraMobile()
+    {
+        useCamera.Translate(Vector3.right * Time.deltaTime * speed);
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Jump()
+    {
+        //cat.Translate(Vector3.up*jump/Time.deltaTime);
+    }
+
+}
---
+++ 2DGame_Run/Assets/EX/ase.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ase : MonoBehaviour
+{
+    [Header("角色屬性")]
+    [Range(1,3),Tooltip("跳次數限制")]public int 跳次數;
+    [Range(1, 10),Tooltip("跑的速度限制")] public float 跑的速度;
+    [Range(1, 2.5f)] public float 跳的高度;
+
+    public Transform 角色,攝影機;
+    
+    private void Update()
+    {
+        跑跑跑();
+        攝影機跑跑跑();
+    }
+
+    void 跑跑跑()
+    {
+        角色.Translate(跑的速度 * Time.deltaTime, 0, 0);
+    }
+
+    void 攝影機跑跑跑()
+    {
+        攝影機.Translate(跑的速度 * Time.deltaTime, 0, 0);
+    }
+
+}
